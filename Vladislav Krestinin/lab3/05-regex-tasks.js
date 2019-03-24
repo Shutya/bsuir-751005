904bca75b2902a058af281e47d34dd7c274812f3
Владислав Крестинин <vlad.krs7@gmail.com>
2019-03-24 21:42:10 +0300

    Update 05-regex-tasks.js

--- Vladislav Krestinin/lab3/05-regex-tasks.js
+++ Vladislav Krestinin/lab3/05-regex-tasks.js
@@ -31,7 +31,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-   throw new Error('Not implemented');
+    return /\{[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}\}/i
 }
 
 
@@ -53,7 +53,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-   throw new Error('Not implemented');
+   return /p.t/
 }
 
 
@@ -72,7 +72,8 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-   throw new Error('Not implemented');
+    let number = '([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])';
+    return new RegExp('^' + number + '(\.' + number + '){3}$');
 }
 
 
@@ -91,7 +92,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-   throw new Error('Not implemented');
+   return /(?!000)\d{3}-(?!00)\d{2}-(?!0000)\d{4}/
 }
 
 
@@ -116,7 +117,7 @@ function getRegexForSSN() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-   throw new Error('Not implemented');
+    return new RegExp("^(?=[A-Za-z0-9]{"+ minLength + ",})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])");
 }
 
 
