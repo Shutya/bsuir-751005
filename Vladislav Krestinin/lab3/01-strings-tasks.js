af6a9bb77d1d5a28ba16a4fa4a807658c4f9f753
Владислав Крестинин <vlad.krs7@gmail.com>
2019-04-13 08:20:47 +0300

    Vladislav Krestinin/feature: Lab 3 done more then 14 days ago
    Renew repo

---
+++ Vladislav Krestinin/lab3/01-strings-tasks.js
@@ -0,0 +1,364 @@
+debugger;
+'use strict';
+
+/********************************************************************************************
+ *                                                                                          *
+ * Plese read the following tutorial before implementing tasks:                             *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String  *
+ *                                                                                          *
+ ********************************************************************************************/
+
+
+
+/**
+ * Returns the result of concatenation of two strings.
+ *
+ * @param {string} value1
+ * @param {string} value2
+ * @return {string}
+ *
+ * @example
+ *   'aa', 'bb' => 'aabb'
+ *   'aa',''    => 'aa'
+ *   '',  'bb'  => 'bb'
+ */
+function concatenateStrings(value1, value2) {
+    return value1 + value2;
+    //throw new Error('Not implemented');
+}
+
+
+/**
+ * Returns the length of given string.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'aaaaa' => 5
+ *   'b'     => 1
+ *   ''      => 0
+ */
+function getStringLength(value) {
+    return value.length;
+    //throw new Error('Not implemented');
+}
+
+/**
+ * Returns the result of string template and given parameters firstName and lastName.
+ * Please do not use concatenation, use template string :
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @return {string}
+ *
+ * @example
+ *   'John','Doe'      => 'Hello, John Doe!'
+ *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
+ */
+function getStringFromTemplate(firstName, lastName) {
+    return `Hello, ${firstName} ${lastName}!`;
+    //throw new Error('Not implemented');
+}
+
+/**
+ * Extracts a name from template string 'Hello, First_Name Last_Name!'.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'Hello, John Doe!' => 'John Doe'
+ *   'Hello, Chuck Norris!' => 'Chuck Norris'
+ */
+function extractNameFromTemplate(value) {
+    return value.slice(value.indexOf(" ") + 1, value.lastIndexOf("!"));
+    //throw new Error('Not implemented');
+}
+
+
+/**
+ * Returns a first char of the given string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'John Doe'  => 'J'
+ *   'cat'       => 'c'
+ */
+function getFirstChar(value) {
+    return value.charAt(0);
+    //throw new Error('Not implemented');
+}
+
+/**
+ * Removes a leading and trailing whitespace characters from string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   '  Abracadabra'    => 'Abracadabra'
+ *   'cat'              => 'cat'
+ *   '\tHello, World! ' => 'Hello, World!'
+ */
+function removeLeadingAndTrailingWhitespaces(value) {
+    return value.trim();
+    //throw new Error('Not implemented');
+}
+
+/**
+ * Returns a string that repeated the specified number of times.
+ *
+ * @param {string} value
+ * @param {string} count
+ * @return {string}
+ *
+ * @example
+ *   'A', 5  => 'AAAAA'
+ *   'cat', 3 => 'catcatcat'
+ */
+function repeatString(value, count) {
+    let i, res = "";
+    for (i = 1; i<=count;i++){
+        res = res + value;
+    }
+    return res;
+
+    //throw new Error('Not implemented');
+}
+
+/**
+ * Remove the first occurrence of string inside another string
+ * 
+ * @param {string} str
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'To be or not to be', 'not'  => 'To be or to be'
+ *   'I like legends', 'end' => 'I like legs',
+ *   'ABABAB','BA' => 'ABAB'
+ */
+function removeFirstOccurrences(str, value) {
+    let substrOccurrence = str.indexOf(value);
+    let res = str;
+    if (~substrOccurrence) {
+        res = res.slice(0, substrOccurrence) + res.slice(substrOccurrence + value.length)
+    }
+    return res;
+    //throw new Error('Not implemented');
+}
+
+/**
+ * Remove the first and last angle brackets from tag string
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   '<div>' => 'div'
+ *   '<span>' => 'span'
+ *   '<a>' => 'a'
+ */
+function unbracketTag(str) {
+    let res = str;
+    let substrOccurrence = res.indexOf('<');
+    if (~substrOccurrence) {
+        res = res.slice(0, substrOccurrence) + res.slice(substrOccurrence + '<'.length)
+    }
+    substrOccurrence = res.indexOf('>');
+    if (~substrOccurrence) {
+        res = res.slice(0, substrOccurrence) + res.slice(substrOccurrence + '>'.length)
+    }
+    return res;
+    //throw new Error('Not implemented');
+}
+
+
+/**
+ * Converts all characters of the specified string into the upper case
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   'Thunderstruck' => 'THUNDERSTRUCK'
+ *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ */
+function convertToUpperCase(str) {
+    return str.toUpperCase();
+    //throw new Error('Not implemented');
+}
+
+/**
+ * Extracts e-mails from single string with e-mails list delimeted by semicolons
+ *
+ * @param {string} str
+ * @return {array}
+ *
+ * @example
+ *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com' => ['angus.young@gmail.com', 'brian.johnson@hotmail.com', 'bon.scott@yahoo.com']
+ *   'info@gmail.com' => ['info@gmail.com']
+ */
+function extractEmails(str) {
+    let emails = [],
+        semicolOccurrence = 0;
+    emails.push(str.slice(semicolOccurrence, ((semicolOccurrence = str.indexOf(';', semicolOccurrence + 1)) >= 0)? semicolOccurrence : str.length));
+    while (~semicolOccurrence){
+        emails.push(str.slice(semicolOccurrence + 1, ((semicolOccurrence = str.indexOf(';', semicolOccurrence + 1)) >= 0)?semicolOccurrence : str.length));
+    }
+    return emails;
+    //throw new Error('Not implemented');
+}
+
+/**
+ * Returns the string representation of rectangle with specified width and height
+ * using pseudograhic chars
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {string}
+ *
+ * @example
+ *
+ *            '┌────┐\n'+
+ *  (6,4) =>  '│    │\n'+
+ *            '│    │\n'+
+ *            '└────┘\n'
+ *
+ *  (2,2) =>  '┌┐\n'+
+ *            '└┘\n'
+ *
+ *             '┌──────────┐\n'+
+ *  (12,3) =>  '│          │\n'+
+ *             '└──────────┘\n'
+ *
+ */
+function getRectangleString(width, height) {
+    let i,j,
+        res ="",
+        newSymb;
+    for (i=1;i<=height;i++){
+        for (j=1;j<=width;j++)
+        {
+            newSymb=' ';
+            if ((i === 1) || (i === height))
+                newSymb  = '─';
+            if ((j === 1) || (j === width))
+                newSymb  = '│';
+            if ((i === 1) && (j === 1))
+                newSymb='┌';
+            else
+                if ((i === 1) && (j === width))
+                    newSymb='┐';
+                else
+                    if ((i === height) && (j === 1))
+                        newSymb='└';
+                    else
+                    if ((i === height) && (j === width))
+                        newSymb='┘';
+            res = res + newSymb;
+        }
+        res = res +'\n';
+    }
+    return res;
+    //throw new Error('Not implemented');
+}
+
+
+/**
+ * Encode specified string with ROT13 cipher
+ * See details:  https://en.wikipedia.org/wiki/ROT13
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *
+ *   'hello' => 'uryyb'
+ *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
+ *   'Gb trg gb gur bgure fvqr!' => 'To get to the other side!'
+ *   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+ *
+ */
+function encodeToRot13(str) {
+    const plainAlphabet  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(''),
+          cipherAlphabet = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'.split(''),
+          substitutionTable = plainAlphabet.reduce((res,currVal,currIndex) => Object.assign(res, {[currVal]: cipherAlphabet[currIndex]}), {});
+    return str.split('').map(x => substitutionTable[x] || x).join('');
+    //throw new Error('Not implemented');
+}
+
+/**
+ * Returns true if the value is string; otherwise false.
+ * @param {string} value
+ * @return {boolean}
+ *
+ * @example
+ *   isString() => false
+ *   isString(null) => false
+ *   isString([]) => false
+ *   isString({}) => false
+ *   isString('test') => true
+ *   isString(new String('test')) => true
+ */
+function isString(value) {
+    return (value instanceof String || (typeof value == 'string'));
+    //throw new Error('Not implemented');
+}
+
+
+/**
+ * Returns playid card id.
+ * 
+ * Playing cards inittial deck inclides the cards in the following order:
+ * 
+ *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+ *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+ *  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+ *  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
+ * 
+ * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
+ * Function returns the zero-based index of specified card in the initial deck above.
+ * 
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'A♣' => 0
+ *   '2♣' => 1 
+ *   '3♣' => 2
+ *     ...
+ *   'Q♠' => 50
+ *   'K♠' => 51
+ */
+function getCardId(value) {
+    const deck  = ['A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+                            'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+                            'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+                            'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'];
+    return deck.indexOf(value);
+    //throw new Error('Not implemented');
+}
+
+
+module.exports = {
+    concatenateStrings: concatenateStrings,
+    getStringLength: getStringLength,
+    getStringFromTemplate: getStringFromTemplate,
+    extractNameFromTemplate: extractNameFromTemplate,
+    getFirstChar: getFirstChar,
+    removeLeadingAndTrailingWhitespaces: removeLeadingAndTrailingWhitespaces,
+    repeatString: repeatString,
+    removeFirstOccurrences: removeFirstOccurrences,
+    unbracketTag: unbracketTag,
+    convertToUpperCase: convertToUpperCase,
+    extractEmails: extractEmails,
+    getRectangleString: getRectangleString,
+    encodeToRot13: encodeToRot13,
+    isString: isString,
+    getCardId: getCardId
+};
